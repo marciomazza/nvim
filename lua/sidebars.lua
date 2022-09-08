ba76a54d78d0427c01b3c2b7ae007f3168bc4572
Marcio Mazza <marciomazza@gmail.com>
2022-09-08 17:47:30 -0300

    Convert sidebars.lua

--- lua/sidebars.lua
+++ lua/sidebars.lua
@@ -1,26 +1,23 @@
-vim.cmd [[
+local set_keymap = require('utils').set_keymap
 
-"" preservim/nerdtree
-let g:NERDTreeChDirMode=2
-let g:NERDTreeIgnore=[
-            \ '\.rbc$', '\~$', '\.pyc$', '\.db$', '\.sqlite$', '.git',
-            \ '__pycache__', '.pytest_cache', '.mypy_cache', 'htmlcov', 'zz*']
-let g:NERDTreeSortOrder=['^__\.py$', '\/$', '*', '\.swp$', '\.bak$', '\~$']
-let g:NERDTreeShowBookmarks=1
-let g:nerdtree_tabs_focus_on_files=1
-let g:NERDTreeMapOpenInTabSilent = '<RightMouse>'
-let g:NERDTreeWinSize = 40
-let g:NERDTreeShowHidden = 1
-let g:NERDTreeQuitOnOpen = 1
-noremap <F3> :NERDTreeToggle<CR>
-"" avoid conflict that show brackets on nerdtree
-""   https://github.com/luochen1990/rainbow/issues/92
-let g:rainbow_conf = { 'separately': { 'nerdtree': 0 } }
+-- preservim/nerdtree
+set_keymap('n', '<F3>', ':NERDTreeToggle<CR>')
+vim.g.NERDTreeChDirMode = 2
+vim.g.NERDTreeIgnore = {'.rbc$', '\\~$', '.pyc$', '.db$', '.sqlite$', '.git',
+                        '__pycache__', '.pytest_cache', '.mypy_cache', 'htmlcov', 'zz*'}
+vim.g.NERDTreeSortOrder = {'^__.py$', '/$', '*', '.swp$', '.bak$', '~$'}
+vim.g.NERDTreeShowBookmarks = 1
+vim.g.nerdtree_tabs_focus_on_files = 1
+vim.g.NERDTreeMapOpenInTabSilent = '<RightMouse>'
+vim.g.NERDTreeWinSize = 40
+vim.g.NERDTreeShowHidden = 1
+vim.g.NERDTreeQuitOnOpen = 1
+-- avoid conflict that show brackets on nerdtree
+-- https://github.com/luochen1990/rainbow/issues/92
+vim.g.rainbow_conf = { separately = { nerdtree = 0 } }
 
-" majutsushi/tagbar
-nnoremap <silent> <F4> :TagbarToggle<CR>
-let g:tagbar_autoclose = 1
-"" sort by position in file
-let g:tagbar_sort = 0
-
-]]
+-- majutsushi/tagbar
+set_keymap('n', '<F4>', ':TagbarToggle<CR>')
+vim.g.tagbar_autoclose = 1
+-- sort by position in file
+vim.g.tagbar_sort = 0
