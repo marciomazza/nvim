f9777e32ac5705cae64fb049330625c0f6f6f816
Marcio Mazza <marciomazza@gmail.com>
2022-09-08 16:17:31 -0300

    Convert files.lua

--- lua/files.lua
+++ lua/files.lua
@@ -1,9 +1,7 @@
-vim.cmd [[
+local set_keymap = require('utils').set_keymap
 
-" grep.vim
-nnoremap <silent> <leader>f :Rgrep<CR>
-let Grep_Default_Options = '-IR'
-let Grep_Skip_Files = '*~ ipython_log.py*'
-let Grep_Skip_Dirs = 'RCS CVS SCCS htmlcov .pytest_cache .mypy_cache zz'
-
-]]
+-- grep.vim
+set_keymap('n', '<leader>f', ':Rgrep<CR>')
+vim.g.Grep_Default_Options = '-IR'
+vim.g.Grep_Skip_Files = '*~ ipython_log.py*'
+vim.g.Grep_Skip_Dirs = 'RCS CVS SCCS htmlcov .pytest_cache .mypy_cache zz'
