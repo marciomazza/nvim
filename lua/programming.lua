f95165f63e597e71564da3a8a143d837d3d13f1c
Marcio Mazza <marciomazza@gmail.com>
2022-09-12 16:20:08 -0300

    Make "gd" jedi goto command work only for python

--- lua/programming.lua
+++ lua/programming.lua
@@ -6,7 +6,32 @@ require'nvim-treesitter.configs'.setup {
   highlight = { enable = true, additional_vim_regex_highlighting = false },
 }
 
--- generall python options
+-- python ----------------------------------------------------------------------
+
+-- davidhalter/jedi-vim
+vim.g['jedi#show_call_signatures'] = "0"
+vim.opt.completeopt:remove {'preview'}  -- disable documentation window
+
+function jedi_goto()
+  -- based on autoload/jedi.vim ... function! jedi#goto()
+  vim.cmd [[
+    python3 jedi_goto_names = jedi_vim.goto(mode="goto")
+    python3 success_lua_boolean = "true" if jedi_goto_names else "false"
+    python3 vim.exec_lua(f"jedi_goto_success = {success_lua_boolean}")
+  ]]
+  return jedi_goto_success
+end
+
+--- extend jedi#goto to fall back to pyimport (useful for names inside comments or under quotes)
+function jedi_goto_or_pyimport()
+  if jedi_goto() then
+    return
+  end
+  -- if goto definition did't work, try pyimport
+  filename = vim.fn['expand']('<cfile>')
+  vim.fn['jedi#py_import'](filename)
+end
+-- general python options
 local python_augroup = vim.api.nvim_create_augroup('python-augroup', {clear = true})
 
 vim.api.nvim_create_autocmd("FileType", {
@@ -23,6 +48,7 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.cmd('iabbrev pdb breakpoint()')  -- TODO make this only local to python files
     -- ajh17/VimCompletesMe: use omnicompletion with tab just on python files
     vim.b.vcm_tab_complete = "omni"
+    vim.keymap.set('n', 'gd', jedi_goto_or_pyimport, {buffer = true})
   end
 })
 
@@ -60,31 +86,3 @@ vim.g.ale_sign_info = '▶'
 -- ALE mappings
 set_keymap('n', '<C-k>', '<Plug>(ale_previous_wrap)')
 set_keymap('n', '<C-j>', '<Plug>(ale_next_wrap)')
-
--- davidhalter/jedi-vim --------------------------------------------------------
-vim.g['jedi#show_call_signatures'] = "0"
-vim.opt.completeopt:remove {'preview'}  -- disable documentation window
-
-function jedi_goto()
-  -- based on ftplugin/python/jedi.vim
-  --   autoload/jedi.vim ... function! jedi#goto()
-  vim.cmd[[
-    python3 jedi_goto_names = jedi_vim.goto(mode="goto")
-    python3 success_lua_boolean = "true" if jedi_goto_names else "false"
-    python3 vim.exec_lua(f"jedi_goto_success = {success_lua_boolean}")
-  ]]
-  return jedi_goto_success
-end
-
---- extend jedi#goto to fall back to pyimport
--- that's useful for names inside comments or under quotes
-function jedi_goto_or_pyimport()
-  if jedi_goto() then
-    return
-  end
-  -- if goto definition did't work, try pyimport
-  filename = vim.fn['expand']('<cfile>')
-  vim.fn['jedi#py_import'](filename)
-end
-
-vim.keymap.set('n', 'gd', jedi_goto_or_pyimport)
