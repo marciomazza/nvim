7944f7f9bb0d37ea1fb86d97a73c57d643c06c10
Marcio Mazza <marciomazza@gmail.com>
2022-09-20 20:13:50 -0300

    Add more nvim-cmp sources

--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,28 +1,41 @@
 
 return {
   'hrsh7th/nvim-cmp',
-  requires = { 'onsails/lspkind.nvim' },
-  config = function()
-    vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-    require('cmp_jedi')
-    local cmp = require('cmp')
-    cmp.setup {
-      mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        -- Accept currently selected item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  requires = { 'onsails/lspkind.nvim',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/cmp-nvim-lua',
+},
+config = function()
+  vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+  require('cmp_jedi')
+  local cmp = require('cmp')
+  cmp.setup {
+    mapping = cmp.mapping.preset.insert({
+      ['<C-Space>'] = cmp.mapping.complete(),
+      -- Accept currently selected item.
+      -- Set `select` to `false` to only confirm explicitly selected items.
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = {
+      { name = 'buffer' },
+      { name = 'path' },
+      { name = 'nvim_lua' },
+      { name = 'jedi' },
+    },
+    formatting = {
+      format = require('lspkind').cmp_format({
+        menu = {
+          jedi = "[jedi]",
+        },
       }),
-      sources = {
-        { name = 'jedi' },
-      },
-      formatting = {
-        format = require('lspkind').cmp_format({
-          menu = {
-            jedi = "[jedi]",
-          },
-        }),
-      },
+    },
+  }
+  cmp.setup.cmdline(':', {
+    sources = {
+      { name = 'cmdline' }
     }
-  end
+  })
+end
 }
