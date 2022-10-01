87de592e90b9c23c714acd514acb4b8140feaf7b
Marcio Mazza <marciomazza@gmail.com>
2022-10-01 15:17:57 -0300

    Update doc about local version of tree-sitter-dockerfile

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -3,19 +3,10 @@
 -- TODO remove this once/if PR is accepted
 -- https://github.com/camdencheek/tree-sitter-dockerfile/pull/24
 --
--- this hack was also needed to get the parser to work
--- https://github.com/nvim-treesitter/nvim-treesitter/discussions/885#discussioncomment-320369
---
--- ln -s \
--- ~/repos/tree-sitter-dockerfile/queries \
--- ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/queries/dockerfile
---
+-- only takes effect after rerunning :TSInstall dockerfile 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.dockerfile = {
-  install_info = {
-    url = "~/repos/tree-sitter-dockerfile",
-    files = { "src/parser.c" },
-  },
+  install_info = { url = "~/repos/tree-sitter-dockerfile", files = { "src/parser.c" } },
 }
 
 return {
