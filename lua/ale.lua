4df891b4d98d92251a822d376ad2331178d620c7
Marcio Mazza <marciomazza@gmail.com>
2023-10-25 19:28:07 -0300

    Use custom ale fixers for lua and django html (with djlint)

--- lua/ale.lua
+++ lua/ale.lua
@@ -1,19 +1,39 @@
+vim.cmd(
+    [[
+function! FormatLua(buffer) abort
+    return {'command': 'luafmt --stdin'}
+endfunction
+
+function! FormatDjlint(buffer) abort
+    return {'command': 'djlint --reformat -'}
+endfunction
+
+execute ale#fix#registry#Add('luafmt', 'FormatLua', ['lua'], 'luafmt for lua')
+execute ale#fix#registry#Add('djlint', 'FormatDjlint', ['html'], 'djlint for html')
+
+]]
+)
+
 -- turn off all linters by default
 vim.g.ale_linters_explicit = 1
 
 -- ALE fixers
 vim.g.ale_fix_on_save = 1
-vim.g.ale_fixers = { ['*'] = {'remove_trailing_lines', 'trim_whitespace'},
-                     python = {'autoflake', 'black', 'isort'}}
+vim.g.ale_fixers = {
+    ["*"] = {"remove_trailing_lines", "trim_whitespace"},
+    python = {"autoflake", "black", "isort"},
+    lua = {"luafmt"},
+    html = {"djlint"}
+}
 vim.g.ale_python_black_use_global = 1
 vim.g.ale_python_isort_use_global = 1
-vim.g.ale_python_isort_options = '--float-to-top --profile black'
+vim.g.ale_python_isort_options = "--float-to-top --profile black"
 
 local disabled = {ale_enabled = 0, ale_fixers = {}}
 
 vim.g.ale_pattern_options = {
-  ['ipython_log.py'] = disabled,
-  ['site-packages'] = disabled,
-  ['plone'] = disabled,
-  ['repos'] = disabled,
+    ["ipython_log.py"] = disabled,
+    ["site-packages"] = disabled,
+    ["plone"] = disabled,
+    ["repos"] = disabled
 }
