490e931f9437e70a6d85bb3b9e60550f04d194d5
Marcio Mazza <marciomazza@gmail.com>
2024-01-05 09:58:08 -0300

    Make tagbar display more compact

--- lua/sidebars.lua
+++ lua/sidebars.lua
@@ -18,6 +18,10 @@ return {
       set_keymap("n", "<F4>", ":TagbarToggle<CR>")
       vim.g.tagbar_autoclose = 1
       vim.g.tagbar_sort = 0 -- sort by position in file
+      vim.g.tagbar_compact = 1
+      -- TODO: start folded all but current symbol
+      --   this should close all folds, but doesn't
+      vim.g.tagbar_foldlevel = 0
     end
   }
 }
