85628ea262543e0b9d618d8914f8d0f48f2d4e18
Marcio Mazza <marciomazza@gmail.com>
2024-01-08 14:49:41 -0300

    Reformat

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 -- enable navigate to lua files with "gf" command
 -- https://www.reddit.com/r/neovim/comments/pl0p5v/comment/hc78cye/?utm_source=share&utm_medium=web2x&context=3
-vim.opt_local.suffixesadd:prepend('.lua')
-vim.opt_local.suffixesadd:prepend('init.lua')
-vim.opt_local.path:prepend(vim.fn.stdpath('config')..'/lua')
+vim.opt_local.suffixesadd:prepend(".lua")
+vim.opt_local.suffixesadd:prepend("init.lua")
+vim.opt_local.path:prepend(vim.fn.stdpath("config") .. "/lua")
