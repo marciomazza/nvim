20ef6fe09a432d06bc675117e9d1a4d9bab86e96
Marcio Mazza <marciomazza@gmail.com>
2024-07-31 16:25:39 -0300

    Add lua snippets

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,12 +5,20 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "hrsh7th/cmp-nvim-lua"
+    "hrsh7th/cmp-nvim-lua",
+    -- lua snippets
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
   },
   config = function()
     vim.opt.completeopt = { "menu", "menuone", "noselect" }
     local cmp = require "cmp"
     cmp.setup {
+      snippet = {
+        expand = function(args)
+          require 'luasnip'.lsp_expand(args.body)
+        end
+      },
       mapping = cmp.mapping.preset.insert(
         {
           ["<C-Space>"] = cmp.mapping.complete(),
@@ -23,7 +31,8 @@ return {
         { name = "nvim_lsp" },
         { name = "buffer" },
         { name = "path" },
-        { name = "nvim_lua" }
+        { name = "nvim_lua" },
+        { name = "luasnip" }
       },
       formatting = { format = require "lspkind".cmp_format() }
     }
