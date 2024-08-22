a89ef17f5c96fbcd097b0e8dfd0f7a5a43d4b574
Marcio Mazza <marciomazza@gmail.com>
2024-08-22 12:57:06 -0300

    Turn off line relative number

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,5 +1,4 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
 -- It breaks a lot of things, so suspended. Perhaps use it in the future
 -- vim.opt.cmdheight = 0  -- autohide the command-line (https://vi.stackexchange.com/a/38231)
 vim.opt.showtabline = 2
