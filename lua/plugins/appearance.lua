16166b7b5026b16528458a2874aa0a1677d314c2
Marcio Mazza <marciomazza@gmail.com>
2024-08-29 17:50:02 -0300

    Hide sign column when not needed

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -2,6 +2,7 @@ vim.opt.number = true      -- enable line numbers in the editor
 vim.opt.showtabline = 2    -- always show the tab line, even if there's only one tab
 vim.wo.colorcolumn = "100" -- set a visual column marker at the 100th character position
 vim.o.scrolloff = 999      -- keep the cursor centered when scrolling
+vim.o.signcolumn = "auto"  -- only show the sign column when there are signs to be displayed
 
 local function noice_skip(pattern)
   return {
