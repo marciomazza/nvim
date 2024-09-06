eff969692644204d2357771da023a0d0705d5bf2
Marcio Mazza <marciomazza@gmail.com>
2024-09-06 09:56:16 -0300

    Add python insert breakpoint shortcut

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,1 +1,10 @@
 vim.opt_local.formatoptions = "croqj"
+
+local function insert_breakpoint()
+  local cursor_pos = unpack(vim.api.nvim_win_get_cursor(0))
+  local current_line = vim.api.nvim_get_current_line()
+  local indent = current_line:match("^%s*")
+  vim.api.nvim_buf_set_lines(0, cursor_pos, cursor_pos, false, { indent .. "breakpoint()" })
+end
+
+vim.keymap.set("n", "<leader>b", insert_breakpoint, { noremap = true, silent = true })
