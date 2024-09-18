bb22c05d0c2d9c68c771d3915f4f5c3a37c7f55e
Marcio Mazza <marciomazza@gmail.com>
2024-09-18 16:47:57 -0300

    Use isort formatter until ruff implements float to top

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -15,6 +15,9 @@ return {
         null_ls.builtins.formatting.djlint.with({ filetypes = djlint_filetypes }),
         null_ls.builtins.diagnostics.djlint.with({ filetypes = djlint_filetypes }),
         null_ls.builtins.formatting.prettier,
+        -- keep this until ruff implements float-to-top
+        --   see https://github.com/astral-sh/ruff/issues/6514
+        null_ls.builtins.formatting.isort.with({ extra_args = { "--float-to-top" } }),
         -- from nvimtools/none-ls-extras.nvim
         require("none-ls.formatting.trim_whitespace"),
         require("none-ls.formatting.trim_newlines"),
