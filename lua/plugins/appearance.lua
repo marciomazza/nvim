b55d5ec28b8ae03ab139ff5e7cf741f7a827468e
Marcio Mazza <marciomazza@gmail.com>
2024-11-11 20:33:36 -0300

    Highlight TODO comments without colon

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -55,6 +55,8 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
-  }
-}
+    opts = {
+      signs = false,
+      highlight = { pattern = [[.*<(KEYWORDS)\s*]] },
+    }
+  } }
