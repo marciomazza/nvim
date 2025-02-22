b75e893eb4dd4a6e7ec5bcf34d66bf4e15e8b8c4
Marcio Mazza <marciomazza@gmail.com>
2025-02-22 14:27:51 -0300

    Improve insert breakpoint function

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,10 +1,12 @@
 vim.opt_local.formatoptions = "croqj"
 
 local function insert_breakpoint()
-  local cursor_pos = unpack(vim.api.nvim_win_get_cursor(0))
-  local current_line = vim.api.nvim_get_current_line()
-  local indent = current_line:match("^%s*")
-  vim.api.nvim_buf_set_lines(0, cursor_pos, cursor_pos, false, { indent .. "breakpoint()" })
+  -- inserts 'breakpoint()' in the line above the cursor with the same indentation
+  local bufnr = 0 -- Buffer atual
+  local line_num = vim.api.nvim_win_get_cursor(0)[1]
+  local line = vim.api.nvim_buf_get_lines(bufnr, line_num - 1, line_num, false)[1]
+  local indent = line:match("^%s*") or ""
+  vim.api.nvim_buf_set_lines(bufnr, line_num - 1, line_num - 1, false, { indent .. "breakpoint()" })
 end
 
 vim.keymap.set("n", "<leader>b", insert_breakpoint, { noremap = true, silent = true })
