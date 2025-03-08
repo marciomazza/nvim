79293e8fb3a04ea7c90237bf92fdd379ce472c6d
Marcio Mazza <marciomazza@gmail.com>
2025-03-07 21:48:19 -0300

    Refactor vimscript to lua

---
+++ after/ftplugin/htmldjango.lua
@@ -0,0 +1,15 @@
+-- TODO
+-- for andymass/vim-matchup to work with django template tags
+-- should work simply by using tweekmonster/django-plus.vim
+-- based on
+-- https://github.com/andymass/vim-matchup/issues/240
+-- https://github.com/tweekmonster/django-plus.vim/blob/master/after/ftplugin/htmldjango.vim#L16
+
+vim.b.match_ignorecase = 1
+vim.b.match_skip = "s:Comment"
+vim.b.match_words = "<:>," ..
+    "<\\@<=[ou]l\\>[^>]*\\%(>\\|$\\):<\\@<=li\\>:<\\@<=/[ou]l>," ..
+    "<\\@<=dl\\>[^>]*\\%(>\\|$\\):<\\@<=d[td]\\>:<\\@<=/dl>," ..
+    "<\\@<=\\([^/][^ \\t>]*\\)[^>]*\\%(>\\|$\\):<\\@<=/\\1>," ..
+    "{% *if .*%}:{% *else *%}:{% *endif *%}," ..
+    "\\%({% *\\)\\@<=\\%(end\\)\\@!\\(\\i\\+\\) .*%}:\\%({% *\\)\\@<=end\\1 .*%}"
