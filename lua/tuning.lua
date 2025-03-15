a18634d7a28798fe1f2b33f4ac60da7bc65fa4ee
Marcio Mazza <marciomazza@gmail.com>
2025-03-15 20:04:53 -0300

    Use vim.keymap.set

--- lua/tuning.lua
+++ lua/tuning.lua
@@ -1,5 +1,3 @@
-local set_keymap = require "utils".set_keymap
-
 -- tolerate some typos on file related commands
 -- apparently, there's no equivalent to the "cnoreabbrev" in lua
 -- https://github.com/nanotee/nvim-lua-guide/issues/37
@@ -38,27 +36,27 @@ vim.opt.expandtab = true
 -- use this otherwise markdown will have these set to 4
 vim.g.markdown_recommended_style = false
 
-set_keymap("n", "<leader><space>", ":noh<cr>") -- clear search highlight
-set_keymap("n", "<Tab>", ":bn<cr>")            -- buffer nav
-set_keymap("n", "<S-Tab>", ":bp<cr>")          -- buffer nav
-set_keymap("n", "<leader>c", ":bd<cr>")        -- close buffer
-set_keymap("n", "<leader>e", ":FZF<cr>")       -- open file with fzf
-set_keymap("n", "<leader>l", ":luafile %<cr>") -- reload lua file
+vim.keymap.set("n", "<leader><space>", ":noh<cr>") -- clear search highlight
+vim.keymap.set("n", "<Tab>", ":bn<cr>")            -- buffer nav
+vim.keymap.set("n", "<S-Tab>", ":bp<cr>")          -- buffer nav
+vim.keymap.set("n", "<leader>c", ":bd<cr>")        -- close buffer
+vim.keymap.set("n", "<leader>e", ":FZF<cr>")       -- open file with fzf
+vim.keymap.set("n", "<leader>l", ":luafile %<cr>") -- reload lua file
 
 -- maintain Visual Mode after shifting > and <
-set_keymap("v", "<", "<gv")
-set_keymap("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 -- toggle spell check
-set_keymap("", "<F6>", ":syntax on<CR>:setlocal spell! spelllang=en_us<CR>")
-set_keymap("", "<F7>", ":syntax on<CR>:setlocal spell! spelllang=pt_br<CR>")
+vim.keymap.set("", "<F6>", ":syntax on<CR>:setlocal spell! spelllang=en_us<CR>")
+vim.keymap.set("", "<F7>", ":syntax on<CR>:setlocal spell! spelllang=pt_br<CR>")
 
 -- hack to suppress "E173: n more files to edit" after :q without visiting all files
 -- https://vi.stackexchange.com/a/31552
 vim.api.nvim_create_autocmd("QuitPre", { command = ":blast" })
 
 -- grep.vim
-set_keymap("n", "<leader>f", ":Rgrep<CR>")
+vim.keymap.set("n", "<leader>f", ":Rgrep<CR>")
 vim.g.Grep_Default_Options = "-IR"
 vim.g.Grep_Skip_Files = "*~ ipython_log.py*"
 vim.g.Grep_Skip_Dirs = "RCS CVS SCCS htmlcov .pytest_cache .mypy_cache zz"
--- lua/utils.lua
+++
@@ -1,8 +0,0 @@
-local utils = {}
-
-function utils.set_keymap(mode, key, result)
-  local options = { noremap = true, silent = true }
-  return vim.api.nvim_set_keymap(mode, key, result, options)
-end
-
-return utils
