a921fbeee2846a4065339df72c6b8e45fa691b70
Marcio Mazza <marciomazza@gmail.com>
2025-08-11 11:19:04 -0300

    refactor

--- lsp/jedi-language-server.lua
+++ lsp/jedi-language-server.lua
@@ -1,47 +1,46 @@
 local ts_utils = require "nvim-treesitter.ts_utils"
 
-local function get_fake_definition_params(module, obj)
+local methods = vim.lsp.protocol.Methods
+
+local fqn_regex = vim.regex([[^\h\w*\(\.\h\w*\)\+$]])
+
+function get_fully_qualified_name_under_cursor()
+  local node = vim.treesitter.get_node():parent():child(1)
+  if not (node and node:type() == "string_content") then
+    return
+  end
+  local text = vim.treesitter.get_node_text(node, 0)
+  if fqn_regex:match_str(text) then
+    return text
+  end
+end
+
+local function build_fake_definition_params(name)
   -- as a trick we prepare a valid python import snippet for the language server
   local uri = "file://" .. vim.fn.tempname() .. ".py"
-  local source = "from " .. module .. " import " .. obj
+  local module, obj = name:match("^(.*)%.([^%.]+)$")
+  local fake_source = string.format("from %s import %s", module, obj)
   local open_params = {
-    textDocument = { uri = uri, languageId = "python", version = 0, text = source }
+    textDocument = { uri = uri, languageId = "python", version = 0, text = fake_source }
   }
   local definition_params = {
-    textDocument = { uri = uri },
-    position = { line = 0, character = #source }
+    textDocument = { uri = uri }, position = { line = 0, character = #fake_source }
   }
   return open_params, definition_params
 end
 
 local function find_name_inside_quotes(client_id)
-  local node = ts_utils.get_node_at_cursor()
-  if not node then
-    return
-  end
-  local type = node:type()
-  -- current node must be at a string
-  if type == "string_start" or type == "string_end" then
-    node = node:parent():child(1) -- move node to string_content
-  end
-  if node:type() ~= "string_content" then
-    return
-  end
-  local name = vim.treesitter.get_node_text(node, 0)
-  -- the text must be a qualified name (e.g. "example.file.name")
-  local module, obj = name:match("^(.*)%.([^%.]+)$")
-  if not module then
-    return
-  end
+  local name = get_fully_qualified_name_under_cursor()
+  if name == nil then return end
   local client = vim.lsp.get_client_by_id(client_id)
-  -- send source to the language server
-  local open_params, definition_params = get_fake_definition_params(module, obj)
-  client.notify("textDocument/didOpen", open_params)
-  local def = client.request_sync("textDocument/definition", definition_params, 1000)
+  if client == nil then return end
+  local open_params, definition_params = build_fake_definition_params(name)
+  client.notify(methods.textDocument_didOpen, open_params)
+  local def = client.request_sync(methods.textDocument_definition, definition_params, 1000)
   return def.result
 end
 
-local default_handler = vim.lsp.handlers["textDocument/definition"]
+local default_handler = vim.lsp.handlers[methods.textDocument_definition]
 
 local function jedi_definition_handler(err, result, ctx, config)
   if result == nil or vim.tbl_isempty(result) then
@@ -58,7 +57,7 @@ return {
   cmd = { "jedi-language-server" },
   filetypes = { "python" },
   root_markers = { "buildout.cfg" }, -- for plone
-  handlers = { ["textDocument/definition"] = jedi_definition_handler },
+  handlers = { [methods.textDocument_definition] = jedi_definition_handler },
   before_init = function(params, _)
     local plone_config = require "plone".get_plone_config()
     if plone_config ~= nil then
