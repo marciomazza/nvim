f16b8db8ee89ed1a5bf26caf25e3faf4c4b37426
Marcio Mazza <marciomazza@gmail.com>
2025-10-09 20:01:14 -0300

    Fix open url under cursor

--- lua/extras.lua
+++ lua/extras.lua
@@ -1,20 +1,9 @@
---- extend "gx" to add github missing prefix
-local function custom_netrw_BrowseX()
-  -- based on netrwPlugin.vim
-  --   see
-  --   /usr/share/nvim/runtime/plugin/netrwPlugin.vim
-  --   /usr/share/nvim/runtime/autoload/netrw.vim
-  local filename = vim.fn["netrw#GX"]() -- gets filename under cursor for netrw
-
-  -- if filename matches a relative github path (like "tpope/vim-surround")
-  if string.match(filename, "^([^/]+/[^/]+)$") then
-    filename = "https://github.com/" .. filename -- adds github prefix
+--- Open URLs under cursor with go (supports GitHub shorthand)
+vim.keymap.set("n", "go", function()
+  local url = vim.fn.expand("<cfile>")
+  -- Handle GitHub shorthand (e.g., "tpope/vim-surround")
+  if url:match("^[%w-_]+/[%w-_.]+$") then
+    url = "https://github.com/" .. url
   end
-
-  -- "gx" originally calls <Plug>NetrwBrowseX, that is:
-  --   netrw#BrowseX(netrw#GX(),netrw#CheckIfRemote(netrw#GX()))<cr>
-  local check_if_remote = vim.fn["netrw#CheckIfRemote"]
-  vim.fn["netrw#BrowseX"](filename, check_if_remote(filename))
-end
-
-vim.keymap.set("n", "gx", custom_netrw_BrowseX)
+  vim.ui.open(url)
+end, { desc = "Open URL under cursor" })
