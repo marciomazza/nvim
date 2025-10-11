87a7fe8f04c7d7da062440edfe97da8ad334f7b5
Marcio Mazza <marciomazza@gmail.com>
2025-10-11 14:23:15 -0300

    Skip format_on_save for more patterns

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -55,10 +55,12 @@ return {
       lsp_format = "fallback",
     },
     format_on_save = function(bufnr)
-      -- skip plone
       local bufname = vim.api.nvim_buf_get_name(bufnr)
-      if bufname:match("/plone/") then
-        return
+      local skip_patterns = { "/plone/", "/node_modules/", "/lib/python" }
+      for _, pattern in ipairs(skip_patterns) do
+        if bufname:match(pattern) then
+          return
+        end
       end
       return {}
     end,
