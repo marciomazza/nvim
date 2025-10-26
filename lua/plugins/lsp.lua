b7ade24acbb5f418fcd5def7a15578bee241e387
Márcio Mazza <marciomazza@gmail.com>
2025-10-26 15:11:43 -0300

    Autoinstall django-template-lsp (djlsp)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,23 +4,38 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
-vim.lsp.enable("djlsp") -- still not known to mason-lspconfig
-
 return {
   {
     "mason-org/mason-lspconfig.nvim",
     dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
+      {
+        "mason-org/mason.nvim",
+        opts = {
+          -- for ensure_installed djlsp (wait until PR merged to remove)
+          -- https://github.com/mason-org/mason-registry/pull/12057
+          registries = {
+            "file:~/repos/mason-registry",
+          },
+        },
+      },
       "neovim/nvim-lspconfig",
     },
     opts = {
-      ensure_installed = { "stylua", "ruff", "jedi_language_server", "lua_ls", "taplo", "tailwindcss", "htmx" },
+      ensure_installed = {
+        "stylua",
+        "ruff",
+        "jedi_language_server",
+        "lua_ls",
+        "taplo",
+        "tailwindcss",
+        "djlsp",
+      },
     },
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
-      ensure_installed = { "prettier", "prettierd", "django-template-lsp" },
+      ensure_installed = { "prettier", "prettierd" },
     },
   },
   {
