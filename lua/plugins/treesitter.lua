90c3ae34cd32451fa0c5f9dd31f81ebe2ee25618
Marcio Mazza <marciomazza@gmail.com>
2025-12-26 09:27:49 -0300

    Fix treesitter config -> opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,30 +11,28 @@ return {
       opts = { max_lines = 1 },
     },
   },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = { "python", "lua", "rust", "javascript", "sql" },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        disable = function(_, _)
-          -- disable treesitter for buffers that are too big (it's too slow)
-          local buffer_size = vim.fn.line2byte(vim.fn.line("$") + 1) - 1
-          return buffer_size > 300 * 1024 -- 300KB
-        end,
+  opts = {
+    ensure_installed = { "python", "lua", "rust", "javascript", "sql" },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      disable = function(_, _)
+        -- disable treesitter for buffers that are too big (it's too slow)
+        local buffer_size = vim.fn.line2byte(vim.fn.line("$") + 1) - 1
+        return buffer_size > 300 * 1024 -- 300KB
+      end,
+    },
+    matchup = { enable = true }, -- enable andymass/vim-matchup
+    -- autotag = { enable = true }, -- enable windwp/nvim-ts-autotag
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<M-Up>",
+        node_incremental = "<M-Up>",
+        node_decremental = "<M-Down>",
       },
-      matchup = { enable = true }, -- enable andymass/vim-matchup
-      -- autotag = { enable = true }, -- enable windwp/nvim-ts-autotag
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<M-Up>",
-          node_incremental = "<M-Up>",
-          node_decremental = "<M-Down>",
-        },
-      },
-      textobjects = { select = { enable = true } },
-    })
-  end,
+    },
+    textobjects = { select = { enable = true } },
+  },
 }
