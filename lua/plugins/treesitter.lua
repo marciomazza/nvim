bbc15f8fd984afa6601ff3d0c7833d19de4a7ea6
Márcio Mazza <marciomazza@gmail.com>
2026-01-19 15:41:40 -0300

    Fix treesitter missing modules after default branch became main
    see https://pawelgrzybek.com/nvim-incremental-selection/

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,36 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  lazy = false,
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    {
-      -- fixme: didn't really solve my issue:
-      -- it's showing just the method context and I wanted the class or both
-      "nvim-treesitter/nvim-treesitter-context",
-      opts = { max_lines = 1 },
-    },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
   },
-  opts = {
-    ensure_installed = { "python", "lua", "rust", "javascript", "sql" },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      disable = function(_, _)
-        -- disable treesitter for buffers that are too big (it's too slow)
-        local buffer_size = vim.fn.line2byte(vim.fn.line("$") + 1) - 1
-        return buffer_size > 300 * 1024 -- 300KB
-      end,
-    },
-    matchup = { enable = true }, -- enable andymass/vim-matchup
-    -- autotag = { enable = true }, -- enable windwp/nvim-ts-autotag
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<M-Up>",
-        node_incremental = "<M-Up>",
-        node_decremental = "<M-Down>",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    -- fixme: didn't really solve my issue:
+    -- it's showing just the method context and I wanted the class or both
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = { max_lines = 1 },
+  },
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    ---@module 'treesitter-modules'
+    ---@type ts.mod.UserConfig
+    opts = {
+      ensure_installed = { "python", "lua", "rust", "javascript", "sql" },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = function(_, _)
+          -- disable treesitter for buffers that are too big (it's too slow)
+          local buffer_size = vim.fn.line2byte(vim.fn.line("$") + 1) - 1
+          return buffer_size > 300 * 1024 -- 300KB
+        end,
+      },
+      matchup = { enable = true }, -- enable andymass/vim-matchup
+      -- autotag = { enable = true }, -- enable windwp/nvim-ts-autotag
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<M-Up>",
+          node_incremental = "<M-Up>",
+          node_decremental = "<M-Down>",
+        },
       },
     },
     textobjects = { select = { enable = true } },
