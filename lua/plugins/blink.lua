e50781e997a75999e90b02d04b401c9386694884
Márcio Mazza <marciomazza@gmail.com>
2026-03-11 12:12:34 -0300

    Adjust blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -9,12 +9,7 @@ return {
   ---@type blink.cmp.Config
   opts = {
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = {
-      preset = "super-tab",
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-    },
+    keymap = { preset = "super-tab" },
 
     sources = {
       -- Remover enabled_providers para permitir todos os sources
@@ -25,7 +20,6 @@ return {
         "buffer",
       },
       per_filetype = { lua = { inherit_defaults = true, "lazydev" } },
-
       providers = {
         lazydev = {
           name = "LazyDev",
@@ -34,6 +28,7 @@ return {
           score_offset = 100,
         },
         lsp = { score_offset = 50 },
+        snippets = { score_offset = 40 },
       },
     },
     fuzzy = { implementation = "rust" },
