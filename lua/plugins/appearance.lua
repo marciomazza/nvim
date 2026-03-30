3e429f0b0cba5fe467dd8ddf70142e933202099a
Márcio Mazza <marciomazza@gmail.com>
2026-03-30 17:25:20 -0300

    Use light theme EdenEast/nightfox.nvim

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -15,23 +15,23 @@ local comment = vim.api.nvim_get_hl(0, { name = "Comment" })
 -- Using Function's foreground color for modified tabs to make them stand out
 local stand_out = vim.api.nvim_get_hl(0, { name = "Function" }).fg
 
-vim.api.nvim_set_hl(0, "MiniTablineCurrent", { bg = normal.bg, fg = normal.fg, bold = true })
-vim.api.nvim_set_hl(0, "MiniTablineVisible", { bg = cursorline.bg, fg = comment.fg })
-vim.api.nvim_set_hl(0, "MiniTablineHidden", { bg = cursorline.bg, fg = comment.fg })
-vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { bg = normal.bg, fg = stand_out, bold = true })
-vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", { bg = cursorline.bg, fg = stand_out })
-vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", { bg = cursorline.bg, fg = stand_out })
-vim.api.nvim_set_hl(0, "MiniTablineFill", { bg = cursorline.bg })
+-- vim.api.nvim_set_hl(0, "MiniTablineCurrent", { bg = normal.bg, fg = normal.fg, bold = true })
+-- vim.api.nvim_set_hl(0, "MiniTablineVisible", { bg = cursorline.bg, fg = comment.fg })
+-- vim.api.nvim_set_hl(0, "MiniTablineHidden", { bg = cursorline.bg, fg = comment.fg })
+-- vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { bg = normal.bg, fg = stand_out, bold = true })
+-- vim.api.nvim_set_hl(0, "MiniTablineModifiedVisible", { bg = cursorline.bg, fg = stand_out })
+-- vim.api.nvim_set_hl(0, "MiniTablineModifiedHidden", { bg = cursorline.bg, fg = stand_out })
+-- vim.api.nvim_set_hl(0, "MiniTablineFill", { bg = cursorline.bg })
 
--- Neogit diff highlights for light theme
-vim.api.nvim_set_hl(0, "NeogitDiffAdd", { bg = "#d4edda", fg = "#1a3a22" })
-vim.api.nvim_set_hl(0, "NeogitDiffAddHighlight", { bg = "#a8d5b5", fg = "#1a3a22" })
-vim.api.nvim_set_hl(0, "NeogitDiffDelete", { bg = "#f8d7da", fg = "#3a1a1a" })
-vim.api.nvim_set_hl(0, "NeogitDiffDeleteHighlight", { bg = "#f1adb3", fg = "#3a1a1a" })
-vim.api.nvim_set_hl(0, "NeogitDiffContext", { bg = "#f5f0e8" })
-vim.api.nvim_set_hl(0, "NeogitDiffContextHighlight", { bg = "#ece6d8" })
-vim.api.nvim_set_hl(0, "NeogitHunkHeader", { bg = "#ddd8cc", fg = "#4a4035", bold = true })
-vim.api.nvim_set_hl(0, "NeogitHunkHeaderHighlight", { bg = "#ccc6b8", fg = "#2a2018", bold = true })
+-- -- Neogit diff highlights for light theme
+-- vim.api.nvim_set_hl(0, "NeogitDiffAdd", { bg = "#d4edda", fg = "#1a3a22" })
+-- vim.api.nvim_set_hl(0, "NeogitDiffAddHighlight", { bg = "#a8d5b5", fg = "#1a3a22" })
+-- vim.api.nvim_set_hl(0, "NeogitDiffDelete", { bg = "#f8d7da", fg = "#3a1a1a" })
+-- vim.api.nvim_set_hl(0, "NeogitDiffDeleteHighlight", { bg = "#f1adb3", fg = "#3a1a1a" })
+-- -- vim.api.nvim_set_hl(0, "NeogitDiffContext", { bg = "#f5f0e8" })
+-- -- vim.api.nvim_set_hl(0, "NeogitDiffContextHighlight", { bg = "#ece6d8" })
+-- vim.api.nvim_set_hl(0, "NeogitHunkHeader", { bg = "#ddd8cc", fg = "#4a4035", bold = true })
+-- vim.api.nvim_set_hl(0, "NeogitHunkHeaderHighlight", { bg = "#ccc6b8", fg = "#2a2018", bold = true })
 
 return {
   "HiPhish/rainbow-delimiters.nvim",
@@ -39,8 +39,11 @@ return {
   {
     "kepano/flexoki-neovim",
     name = "flexoki",
+  },
+  {
+    "EdenEast/nightfox.nvim",
     init = function()
-      vim.cmd.colorscheme("flexoki-light")
+      vim.cmd.colorscheme("dayfox")
     end,
   },
 }
