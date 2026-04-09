41ddaa8597981e8f0a5f051e475dc03e1c528d67
Márcio Mazza <marciomazza@gmail.com>
2026-04-09 10:32:06 -0300

    Add <leader>mp shortcut for MarkdownPreview

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -2,6 +2,7 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    keys = { { "<leader>mp", vim.cmd.MarkdownPreview, desc = "Markdown Preview" } },
     build = "cd app && npm install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
