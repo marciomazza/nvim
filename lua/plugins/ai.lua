1d63e005474d6a331a59f2da9d34e7fcded617a9
Márcio Mazza <marciomazza@gmail.com>
2026-04-14 08:48:01 -0300

    Adjust blink + copilot

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -4,17 +4,7 @@ return {
     cmd = "Copilot",
     event = "InsertEnter",
     opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        hide_during_completion = true,
-        keymap = {
-          accept = "<Tab>",
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<M-e>",
-        },
-      },
+      suggestion = { enabled = false },
       panel = { enabled = false },
     },
   },
--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,23 +3,53 @@ return {
   version = "1.*",
   dependencies = {
     "rafamadriz/friendly-snippets",
+    "giuxtaposition/blink-cmp-copilot",
   },
 
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = "default" },
+    keymap = { preset = "super-tab" },
+    completion = {
+      list = {
+        selection = { auto_insert = false },
+      },
+      menu = { border = "rounded" },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+        window = { border = "rounded" },
+      },
+      ghost_text = { enabled = true },
+    },
+    signature = {
+      enabled = true,
+      window = { border = "rounded" },
+    },
+    appearance = {
+      use_nvim_cmp_as_default = true,
+      kind_icons = {
+        Copilot = "",
+      },
+    },
     sources = {
       -- Remover enabled_providers para permitir todos os sources
       default = {
         "lsp",
         "path",
-        -- "snippets",
+        "snippets",
         "buffer",
+        "copilot",
       },
       per_filetype = { lua = { inherit_defaults = true, "lazydev" } },
       providers = {
+        copilot = {
+          name = "copilot",
+          module = "blink-cmp-copilot",
+          score_offset = 100,
+          async = true,
+        },
         lazydev = {
           name = "LazyDev",
           module = "lazydev.integrations.blink",
@@ -27,10 +57,10 @@ return {
           score_offset = 100,
         },
         lsp = { score_offset = 50 },
-        -- snippets = { score_offset = 40 },
+        snippets = { score_offset = 40 },
+        buffer = { score_offset = 30, min_keyword_length = 2 },
       },
     },
-    fuzzy = { implementation = "rust" },
   },
   opts_extend = { "sources.default" },
 }
