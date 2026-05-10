de4ded19a62f058047db5978da5955d906c2bce1
Márcio Mazza <marciomazza@gmail.com>
2026-05-09 21:19:37 -0300

    Use snacks file rename with mini.files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,8 @@
+vim.api.nvim_create_autocmd("User", {
+	pattern = "MiniFilesActionRename",
+	callback = function(event) Snacks.rename.on_rename_file(event.data.from, event.data.to) end,
+})
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
