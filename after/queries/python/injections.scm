7e5eb6509a212d4b7efad4f6cfbb6c4c248f130d
Márcio Mazza <marciomazza@gmail.com>
2026-06-29 14:58:27 -0300

    Add fixture support to js in py injections

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -2,10 +2,11 @@
 ;
 ; JavaScript injections for Python files.
 ; Enables JS syntax highlighting inside Python strings that contain JavaScript,
-; covering three patterns used in this codebase:
+; covering four patterns used in this codebase:
 ;   1. Strings assigned to a variable named `js`
 ;   2. String literals passed directly to .eval() or .eval_async()
-;   3. Strings returned from functions whose name ends in `_js`
+;   3. Strings returned from functions whose name ends in `js`
+;   4. pytest.mark.parametrize lists where a fixture name ends in `js`
 
 ; Inject JavaScript into strings assigned to a variable named `js`
 (assignment
@@ -25,12 +26,61 @@
       (string_content) @injection.content))
   (#set! injection.language "javascript"))
 
-; Inject into strings returned from functions whose name ends in _js
+; Inject into strings returned from functions whose name ends in js
 (function_definition
   name: (identifier) @_fn
-  (#match? @_fn "_js$")
+  (#match? @_fn "js$")
   body: (block
     (return_statement
       (string
         (string_content) @injection.content))
     (#set! injection.language "javascript")))
+
+; --------------------------------------------------------------------------------
+; pytest.mark.parametrize
+; --------------------------------------------------------------------------------
+
+; Inject into parametrize list strings when the fixture name ends in js
+; Single fixture: direct strings in the list
+(decorator
+  (call
+    arguments: (argument_list
+      (string (string_content) @_name (#match? @_name "^[^,]*js\\s*$"))
+      (list
+        (string
+          (string_content) @injection.content))))
+  (#set! injection.language "javascript"))
+
+; Multiple fixtures, js is first: capture first string in each tuple/list
+(decorator
+  (call
+    arguments: (argument_list
+      (string (string_content) @_name (#match? @_name "^[^,]*js\\s*,"))
+      (list
+        (_
+          . (string (string_content) @injection.content)
+          (_)))))
+  (#set! injection.language "javascript"))
+
+; Multiple fixtures, js is middle: capture middle string in each tuple/list
+(decorator
+  (call
+    arguments: (argument_list
+      (string (string_content) @_name (#match? @_name ",\\s*[^,]*js\\s*,"))
+      (list
+        (_
+          (_)
+          (string (string_content) @injection.content)
+          (_)))))
+  (#set! injection.language "javascript"))
+
+; Multiple fixtures, js is last: capture last string in each tuple/list
+(decorator
+  (call
+    arguments: (argument_list
+      (string (string_content) @_name (#match? @_name ",\\s*[^,]*js\\s*$"))
+      (list
+        (_
+          (_)
+          (string (string_content) @injection.content) .))))
+  (#set! injection.language "javascript"))
