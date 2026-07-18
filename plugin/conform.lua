20145ba778550f4ce666d30c38ad719afde47621
Márcio Mazza <marciomazza@gmail.com>
2026-07-18 14:17:55 -0300

    Remove our float imports to top hack since ruff 0.15.22 now implements it

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -2,37 +2,6 @@ vim.pack.add({
   "https://github.com/stevearc/conform.nvim",
 })
 
-local function find_import_insertion_line_number(bufnr)
-  local root = vim.treesitter.get_parser(bufnr):trees()[1]:root()
-  for node in root:iter_children() do
-    if node:type() == "comment" then
-      -- skip initial comments, that might come before the module docstring
-    elseif node:type() == "expression_statement" and node:child(0):type() == "string" then
-      -- found a module docstring => imports should start at the next line
-      local _, _, line_number, _ = node:range()
-      return line_number + 2 -- treesitter ranges are 0 based => add 1 more
-    else
-      return 1 -- no module docstring => imports should start at the very top
-    end
-  end
-end
-
-local utils = require("utils")
-
-local function float_imports_to_top(bufnr, lines)
-  local misplaced_imports = vim
-    .iter(vim.diagnostic.get(bufnr))
-    :filter(function(d) return d.source == "Ruff" and d.user_data.lsp.code == "E402" end)
-    :map(function(d) return vim.list_slice(lines, d.lnum + 1, d.end_lnum + 1) end)
-    :totable()
-  if #misplaced_imports == 0 then return end
-  local insertion_index = find_import_insertion_line_number(bufnr)
-  for _, block in ipairs(misplaced_imports) do
-    utils.move_sublist(lines, block, insertion_index)
-    insertion_index = insertion_index + #block
-  end
-end
-
 local function python_pre_injected(self, ctx, lines, callback)
   local query = vim.treesitter.query.get("python", "injections")
   if not query then return callback(nil, lines) end
@@ -150,7 +119,6 @@ require("conform").setup({
       "python_pre_injected",
       "injected",
       "python_post_injected",
-      "float_imports_to_top",
       "ruff_fix",
       "ruff_format",
     },
@@ -188,13 +156,6 @@ require("conform").setup({
     oxfmt_injected = {
       inherit = "oxfmt",
       append_args = { "-c", vim.fn.stdpath("config") .. "/.oxfmtrc.injected.json" },
-    },
-    float_imports_to_top = {
-      -- keep this until ruff implements float-to-top (https://github.com/astral-sh/ruff/issues/6514)
-      format = function(_, ctx, lines, callback)
-        float_imports_to_top(ctx.buf, lines)
-        callback(nil, lines)
-      end,
     },
     ruff_fix = {
       append_args = { "--unsafe-fixes" },
