78986fc831cb105ba5ee57d0e66a774dc24db1aa
Márcio Mazza <marciomazza@gmail.com>
2026-08-30 19:28:06 -0300

    Format JavaScript injected in Rust strings via conform

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -148,7 +148,7 @@ local for_htmldjango = { "rustywind", "djangofmt" }
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    rust = { "rustfmt" },
+    rust = { "rustfmt", "injected" },
     python = {
       "python_pre_injected",
       "injected",
