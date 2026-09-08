ebcbb1fe8e489a0c9cec9d96f9c5d0e92132f3bf
Márcio Mazza <marciomazza@gmail.com>
2026-09-08 10:38:19 -0300

    Skip format on save for extra/hc/h* projects

--- plugin/conform.lua
+++ plugin/conform.lua
@@ -338,7 +338,7 @@ require("conform").setup({
     lsp_format = "fallback",
   },
   format_after_save = function(bufnr)
-    local skip_patterns = { "/plone/", "/node_modules/", "/lib/python", "/repos/", "/dist/debug/" }
+    local skip_patterns = { "/plone/", "/node_modules/", "/lib/python", "/repos/", "/dist/debug/", "/extra/hc/h" }
     local bufname = vim.api.nvim_buf_get_name(bufnr)
     local skip = vim.iter(skip_patterns):any(function(pattern) return bufname:match(pattern) end)
     return not skip and {}
